3da2c153f72be6465a8e29aa815f5e5240c3ac2a
Michael Kreil <github@michael-kreil.de>
2023-03-07 20:21:17 +0100

    add process_styles

--- bin/generate.sh
+++ bin/generate.sh
@@ -6,27 +6,36 @@ cd ..
 
 echo " -> prepare folders"
 rm -rf dist || true
+mkdir dist
+cd dist
 
-mkdir -p dist/frontend/
-cd dist/frontend/
+assets="frontend/assets"
+mkdir -p $assets
 
 echo " -> add fonts"
-mkdir -p assets/fonts
-curl -L --no-progress-meter "https://github.com/versatiles-org/versatiles-fonts/releases/latest/download/fonts.tar.gz" | gzip -d | tar -xf - -C assets/fonts/
+mkdir -p $assets/fonts
+font_names=(noto_sans)
+for i in "${font_names[@]}"
+do
+	fontname=${font_names[$i]}
+	echo "    -> add $fontname"
+	curl -L --no-progress-meter "https://github.com/versatiles-org/versatiles-fonts/releases/latest/download/$fontname.tar.gz" | gzip -d | tar -xf - -C $assets/fonts/
+done
 
 echo " -> add styles"
-mkdir -p assets/styles
-curl -L --no-progress-meter "https://github.com/versatiles-org/versatiles-styles/releases/latest/download/styles.tar.gz" | gzip -d | tar -xf - -C assets/styles/
+mkdir -p $assets/styles
+curl -L --no-progress-meter "https://github.com/versatiles-org/versatiles-styles/releases/latest/download/styles.tar.gz" | gzip -d | tar -xf - -C $assets/styles/
+../bin/process_styles.js $assets/styles/
 
 echo " -> add maplibre"
-mkdir -p assets/maplibre
+mkdir -p $assets/maplibre
 curl -L --no-progress-meter -o maplibre.zip https://github.com/maplibre/maplibre-gl-js/releases/latest/download/dist.zip
-unzip -q maplibre.zip
-cp dist/*.js assets/maplibre/
-cp dist/*.css assets/maplibre/
-cp dist/*.map assets/maplibre/
-rm -r dist
+unzip -q -d tmp maplibre.zip
+cp tmp/*.js $assets/maplibre/
+cp tmp/*.css $assets/maplibre/
+cp tmp/*.map $assets/maplibre/
+rm -r tmp
 rm maplibre.zip
 
 echo " -> add frontend"
-cp -r ../../src/* .
+cp -r ../src/* frontend/
---
+++ bin/process_styles.js
@@ -0,0 +1,194 @@
+#!/usr/bin/env node
+
+import process from 'node:process';
+import { existsSync, readdirSync, readFileSync, statSync, writeFileSync } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+
+
+let path = process.argv[2];
+if (!path) {
+	console.error('ERROR - need one argument: the name of style path');
+	process.exit(1);
+}
+
+path = resolve(process.cwd(), path);
+if (!existsSync(path)) {
+	console.error(`ERROR - path "${path}" not found`);
+	process.exit(1);
+}
+
+const fontsPath = resolve(path, '../fonts');
+let knownFontNames = new Set();
+readdirSync(fontsPath).forEach(subFolder => {
+	if (!statSync(resolve(fontsPath, subFolder)).isDirectory()) return;
+	knownFontNames.add(subFolder);
+})
+
+
+
+readdirSync(path).forEach(subFolder => {
+	if (subFolder.startsWith('.')) return;
+
+	let stylePath = resolve(path, subFolder);
+	let styleFilename = resolve(stylePath, 'style.json');
+	if (!existsSync(styleFilename)) {
+		console.error(`ERROR - style "${styleFilename}" not found`);
+		process.exit(1);
+	}
+
+	let style = JSON.parse(readFileSync(styleFilename, 'utf8'));
+	console.log('process', subFolder);
+
+	validateAndPatchStyle();
+	writeFileSync(resolve(stylePath, 'style.min.json'), JSON.stringify(style));
+	writeFileSync(resolve(stylePath, 'style.json'), JSON.stringify(style, null, '\t'));
+	saveWrapped(resolve(stylePath, 'style.js'))
+
+
+
+	function validateAndPatchStyle() {
+
+		checkMeta();
+		fixPaths();
+		fixFontNames();
+		checkSprites();
+
+		function checkMeta() {
+			if (style.metadata.license !== 'https://creativecommons.org/publicdomain/zero/1.0/') throw Error();
+		}
+
+		function fixPaths() {
+			// don't reference glyphs
+			delete style.glyphs;
+
+			// don't reference tile source
+			Object.values(style.sources).forEach(source => {
+				delete source.tiles;
+			})
+
+			if (style.sprites) style.sprites = './sprite';
+		}
+
+		function checkSprites() {
+			let knownSprites = new Set();
+			if (style.sprites) {
+				let sprites = JSON.parse(readFileSync(resolve(stylePath, style.sprites + '.json'), 'utf8'));
+				Object.keys(sprites).forEach(name => knownSprites.add(name));
+			}
+
+			let attributes = 'background-pattern,fill-extrusion-pattern,fill-pattern,icon-image,line-pattern'.split(',');
+			style.layers.forEach(layer => {
+				if (!layer.paint) return;
+				attributes.forEach(attribute => {
+					if (!layer.paint[attribute]) return;
+					throw Error('implement "resolvedImage" checking');
+				})
+			})
+		}
+
+		function fixFontNames() {
+			style.layers.forEach(layer => {
+				if (!layer.layout) return;
+				if (!layer.layout['text-font']) return;
+
+				let list = layer.layout['text-font'];
+				if (!Array.isArray(list)) throw Error('must be an array');
+				for (let i = 0; i < list.length; i++) {
+					if (typeof list[i] !== 'string') throw Error('must be an array of strings');
+
+					list[i] = list[i].toLowerCase().replace(/\s+/g, '_');
+
+					if (!knownFontNames.has(list[i])) {
+						throw Error(`unknown font name "${list[i]}"`)
+					}
+				}
+			})
+		}
+	}
+
+	function saveWrapped(filename) {
+		// use https://www.npmjs.com/package/colord
+	}
+})
+
+
+/*
+
+
+
+
+// -------------------------------------------------------------------------------------
+
+
+
+async function validateStyle(style, path) {
+	let knownFontNames = readFileSync(resolve(__dirname, '../src/known_fonts.txt'), 'utf8');
+	knownFontNames = knownFontNames.split('\n').map(f => f.replace(/[^a-z_]+/g, '')).filter(f => f.length > 2);
+	knownFontNames = new Set(knownFontNames);
+
+	checkMeta();
+	fixPaths();
+	fixFontNames();
+	checkSprites();
+
+	async function checkMeta() {
+		if (style.metadata.license !== 'https://creativecommons.org/publicdomain/zero/1.0/') throw Error();
+	}
+
+	function fixPaths() {
+		// don't reference glyphs
+		delete style.glyphs;
+
+		// don't reference tile source
+		Object.values(style.sources).forEach(source => {
+			delete source.tiles;
+		})
+
+		if (style.sprites) style.sprites = './sprite';
+	}
+
+	function checkSprites() {
+		let knownSprites = new Set();
+		if (style.sprites) {
+			let sprites = JSON.parse(readFileSync(resolve(path, style.sprites + '.json'), 'utf8'));
+			Object.keys(sprites).forEach(name => knownSprites.add(name));
+		}
+
+		let attributes = 'background-pattern,fill-extrusion-pattern,fill-pattern,icon-image,line-pattern'.split(',');
+		style.layers.forEach(layer => {
+			if (!layer.paint) return;
+			attributes.forEach(attribute => {
+				if (!layer.paint[attribute]) return;
+				throw Error('implement "resolvedImage" checking');
+			})
+		})
+	}
+
+	function fixFontNames() {
+		style.layers.forEach(layer => {
+			if (!layer.layout) return;
+			if (!layer.layout['text-font']) return;
+
+			let list = layer.layout['text-font'];
+			if (!Array.isArray(list)) throw Error('must be an array');
+			for (let i = 0; i < list.length; i++) {
+				if (typeof list[i] !== 'string') throw Error('must be an array of strings');
+
+				list[i] = list[i].toLowerCase().replace(/\s+/g, '_');
+
+				if (!knownFontNames.has(list[i])) {
+					throw Error(`unknown font name "${list[i]}"`)
+				}
+			}
+		})
+	}
+}
+
+function saveMinified(style, filename) {
+	writeFileSync(filename, JSON.stringify(style), 'utf8');
+}
+
+function saveWrapped() {
+	// use https://www.npmjs.com/package/colord
+}
+*/
