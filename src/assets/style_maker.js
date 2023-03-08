9de83273ee5ae51d506e3392fec5cc12e0845244
Michael Kreil <github@michael-kreil.de>
2023-03-08 14:25:01 +0100

    remove unused code

--- src/assets/style_maker.js
+++ src/assets/style_maker.js
@@ -138,6 +138,18 @@ function makeStyle(style, tileSource, options) {
 				b = 255-b;
 			}
 
+			if (options.hueRotate) {
+				let c = rgbToHsv(r, g, b);
+				c[0] = (c[0] + 360*options.hueRotate) % 360;
+
+				options.hueRotate
+				let c2 = rgbToHsv(...options.tintColor);
+				let c4 = hsvToRgb(c2[0], c2[1] * c1[1] / 100, c1[2]);
+				r = r * (1 - options.tint) + c4[0] * options.tint;
+				g = g * (1 - options.tint) + c4[1] * options.tint;
+				b = b * (1 - options.tint) + c4[2] * options.tint;
+			}
+
 			if (options.fade) {
 				r = r * (1 - options.fade) + options.fade * options.fadeColor[0];
 				g = g * (1 - options.fade) + options.fade * options.fadeColor[1];
@@ -198,108 +210,5 @@ function makeStyle(style, tileSource, options) {
 				[b, b, d, v, v, c][module] * 255
 			]
 		}
-	}
-}
-
-
-
-
-
-
-async function styleMaker(styleUrl, tilesPathTemplate, options) {
-	let baseUrl = window.location.href
-	let style = await (await fetch(styleUrl)).json();
-
-	style.sprite = absoluteUrl(styleUrl, style.sprite);
-	style.glyphs = absoluteUrl(styleUrl, style.glyphs);
-	style.sources.versatiles.tiles = [absoluteUrl(tilesPathTemplate)];
-
-	if (options) {
-		patchLayers(style.layers);
-	}
-
-	function absoluteUrl(...urls) {
-		// use encodeURI/decodeURI to handle curly brackets in path templates
-		let url = encodeURI(baseUrl);
-		while (urls.length > 0) {
-			url = (new URL(encodeURI(urls.shift()), url)).href;
-		}
-		return decodeURI(url);
-	}
-
-	return style;
-}
-
-
-function patchLayers(layers, options = { grey: 0, pale: 0, invert: false }) {
-	let paintColorKeys = [
-		'background-color',
-		'circle-color',
-		'circle-stroke-color',
-		'fill-color',
-		'fill-extrusion-color',
-		'fill-outline-color',
-		'heatmap-color',
-		'hillshade-accent-color',
-		'hillshade-highlight-color',
-		'hillshade-shadow-color',
-		'icon-color',
-		'icon-halo-color',
-		'line-color',
-		'line-gradient',
-		'sky-atmosphere-color',
-		'sky-atmosphere-halo-color',
-		'sky-gradient',
-		'text-color',
-		'text-halo-color',
-	]
-
-	layers.forEach(layer => {
-		paintColorKeys.forEach(key => {
-			if (layer.paint[key]) layer.paint[key] = fixColorValue(layer.paint[key]);
-		})
-	})
-
-	function fixColorValue(value) {
-		if (typeof value === 'string') return repairColor(value);
-		console.log('fixColorValue', value);
-		throw new Error(value);
-	}
-
-	function repairColor(color) {
-		let r, g, b;
-
-		if (/^#[0-9a-f]{6}$/i.test(color)) {
-			r = parseInt(color.slice(1, 3), 16);
-			g = parseInt(color.slice(3, 5), 16);
-			b = parseInt(color.slice(5, 7), 16);
-		} else {
-			throw new Error(color);
-		}
-
-		if (options.grey) {
-			let m = opt.grey * (r * 0.299 + g * 0.587 + b * 0.114);
-			let f = 1 - opt.grey;
-			r = r * f + m;
-			g = g * f + m;
-			b = b * f + m;
-		}
-
-		if (options.pale) {
-			r = 255 - (255 - r) * (1 - options.pale);
-			g = 255 - (255 - g) * (1 - options.pale);
-			b = 255 - (255 - b) * (1 - options.pale);
-		}
-
-		if (options.invert) {
-			let m = 255 - Math.max(r, g, b) - Math.min(r, g, b);
-			r += m;
-			g += m;
-			b += m;
-		}
-
-		let result = '#' + [r, g, b].map(v => ('00' + Math.round(v).toString(16)).slice(-2)).join('');
-		//console.log(color, r, g, b, result);
-		return result;
 	}
 }
