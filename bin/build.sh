2894c767afa0863180dd540d0008210c164be435
Michael Kreil <github@michael-kreil.de>
2023-08-08 19:55:37 +0200

    add release notes

--- bin/build.sh
+++ bin/build.sh
@@ -39,3 +39,20 @@ rm maplibre.zip
 
 echo " -> add frontend"
 cp -r ../src/* frontend/
+
+echo " -> make release notes"
+
+V_FONTS=$(curl -s https://api.github.com/repos/versatiles-org/versatiles-fonts/tags | jq -r 'first(.[] | .name | select(startswith("v")))')
+V_STYLES=$(curl -s https://api.github.com/repos/versatiles-org/versatiles-styles/tags | jq -r 'first(.[] | .name | select(startswith("v")))')
+V_SPRITES=$(curl -s https://api.github.com/repos/versatiles-org/versatiles-sprites/tags | jq -r 'first(.[] | .name | select(startswith("v")))')
+V_FRONTEND=v$(jq -r '.version' ../package.json)
+V_MAPLIBRE=$(curl -s https://api.github.com/repos/maplibre/maplibre-gl-js/tags | jq -r 'first(.[] | .name | select(startswith("v")))')
+
+echo "" > notes.md
+echo "# VersaTiles Frontend $V_FRONTEND" >> notes.md
+echo "" >> notes.md
+echo "includes:" >> notes.md
+echo "- MapLibre GL JS $V_MAPLIBRE" >> notes.md
+echo "- VersaTiles Fonts $V_FONTS" >> notes.md
+echo "- VersaTiles Sprites $V_SPRITES" >> notes.md
+echo "- VersaTiles Styles $V_STYLES" >> notes.md
