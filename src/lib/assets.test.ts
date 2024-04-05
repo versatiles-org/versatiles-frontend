5b37d1ca0f733e4a9e3d0e772183425ec205e11f
Michael Kreil <github@michael-kreil.de>
2024-04-05 11:24:52 +0200

    test: update getAssets

--- src/lib/assets.test.ts
+++ src/lib/assets.test.ts
@@ -35,25 +35,26 @@ describe('getAssets', () => {
 
 		await expect(PromiseFunction.run(getAssets(mockFileSystem))).resolves.toBeUndefined();
 
+		// test if correct release versions were requestes
 		const glrvCalls = jest.mocked(getLatestReleaseVersion).mock.calls;
 		glrvCalls.sort((a, b) => a[0].localeCompare(b[0]) || a[1].localeCompare(b[1]));
 		expect(glrvCalls).toStrictEqual([
-			['maplibre', 'maplibre-gl-inspect'],
 			['maplibre', 'maplibre-gl-js'],
 			['versatiles-org', 'versatiles-fonts'],
 			['versatiles-org', 'versatiles-style'],
 		]);
 
+		// test if correct releases were downloaded
 		const curlResults = jest.mocked(Curl).mock.results.map(e => (e.value as { url: string }).url);
 		curlResults.sort();
 		expect(curlResults).toStrictEqual([
-			'https://github.com/maplibre/maplibre-gl-inspect/releases/download/v1.2.3/maplibre-gl-inspect.css',
-			'https://github.com/maplibre/maplibre-gl-inspect/releases/download/v1.2.3/maplibre-gl-inspect.js',
 			'https://github.com/maplibre/maplibre-gl-js/releases/download/v1.2.3/dist.zip',
 			'https://github.com/versatiles-org/versatiles-fonts/releases/download/v1.2.3/fonts.tar.gz',
 			'https://github.com/versatiles-org/versatiles-style/releases/download/v1.2.3/sprites.tar.gz',
 			'https://github.com/versatiles-org/versatiles-style/releases/download/v1.2.3/styles.tar.gz',
 			'https://github.com/versatiles-org/versatiles-style/releases/download/v1.2.3/versatiles-style.tar.gz',
+			'https://unpkg.com/@maplibre/maplibre-gl-inspect@latest/dist/maplibre-gl-inspect.css',
+			'https://unpkg.com/@maplibre/maplibre-gl-inspect@latest/dist/maplibre-gl-inspect.js',
 		]);
 	});
 });
