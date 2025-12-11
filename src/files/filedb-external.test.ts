616d6b40ccf8d8c0267b07407701b78489032d1c
Michael Kreil <github@michael-kreil.de>
2025-12-11 15:03:18 +0100

    refactor: enhance progress module mocking in filedb-external.test.ts

--- src/files/filedb-external.test.ts
+++ src/files/filedb-external.test.ts
@@ -1,7 +1,57 @@
 import { vi, describe, it, expect, beforeEach } from 'vitest';
+import type { ProgressLabel as ProgressLabelType, Progress as ProgressType } from '../async_progress/progress';
 
 // Mock progress module
-vi.mock('../utils/progress', async () => await import('../utils/__mocks__/progress'));
+vi.mock('../async_progress/progress', async (originalImport) => {
+	const originalModule = (await originalImport()) as typeof import('../async_progress/progress');
+	originalModule.default.disable();
+
+	function mockProgressLabel(progressLabel: ProgressLabelType) {
+		vi.spyOn(progressLabel, 'updateLabel');
+		vi.spyOn(progressLabel, 'start');
+		vi.spyOn(progressLabel, 'end');
+		vi.spyOn(progressLabel, 'getOutputAnsi');
+		vi.spyOn(progressLabel, 'getOutputText');
+	}
+
+	class ProgressLabel extends originalModule.ProgressLabel {
+		constructor(progress: ProgressType, label: string, indent: number) {
+			super(progress, label, indent);
+			mockProgressLabel(this);
+		}
+	}
+
+	class Progress extends originalModule.Progress {
+		constructor() {
+			super();
+
+			// Wrap the original add method so we can spy on the returned ProgressLabel as well
+			const originalAdd = this.add.bind(this);
+			this.add = ((name: string, indent = 0): ProgressLabelType => {
+				const progressLabel = originalAdd(name, indent);
+				mockProgressLabel(progressLabel);
+				return progressLabel;
+			}) as ProgressType['add'];
+		}
+	}
+
+	const progress = new Progress();
+	vi.spyOn(progress, 'add');
+	vi.spyOn(progress, 'disable');
+	vi.spyOn(progress, 'finish');
+	vi.spyOn(progress, 'redraw');
+	vi.spyOn(progress, 'setAnsi');
+	vi.spyOn(progress, 'setHeader');
+	vi.spyOn(progress, 'write');
+
+	return {
+		Progress: vi.fn(function () {
+			return progress;
+		}),
+		default: progress,
+		ProgressLabel,
+	};
+});
 
 // Mock curl module
 vi.mock('../utils/curl', async () => await import('../utils/__mocks__/curl'));
