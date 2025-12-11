908e79bfc0bec9a87c3a3ac875f31cae62a19087
Michael Kreil <github@michael-kreil.de>
2025-12-11 15:02:48 +0100

    refactor: remove progress module mock file and integrate mocking directly in async.test.ts

--- src/async_progress/async.test.ts
+++ src/async_progress/async.test.ts
@@ -1,6 +1,58 @@
 import { vi, describe, it, expect, Mock } from 'vitest';
-import './__mocks__/progress';
-import type { ProgressLabel } from './progress';
+import type { ProgressLabel, ProgressLabel as ProgressLabelType, Progress as ProgressType } from './progress';
+
+// Mock progress module
+vi.mock('./progress', async (originalImport) => {
+	const originalModule = (await originalImport()) as typeof import('./progress');
+	originalModule.default.disable();
+
+	function mockProgressLabel(progressLabel: ProgressLabelType) {
+		vi.spyOn(progressLabel, 'updateLabel');
+		vi.spyOn(progressLabel, 'start');
+		vi.spyOn(progressLabel, 'end');
+		vi.spyOn(progressLabel, 'getOutputAnsi');
+		vi.spyOn(progressLabel, 'getOutputText');
+	}
+
+	class ProgressLabel extends originalModule.ProgressLabel {
+		constructor(progress: ProgressType, label: string, indent: number) {
+			super(progress, label, indent);
+			mockProgressLabel(this);
+		}
+	}
+
+	class Progress extends originalModule.Progress {
+		constructor() {
+			super();
+
+			// Wrap the original add method so we can spy on the returned ProgressLabel as well
+			const originalAdd = this.add.bind(this);
+			this.add = ((name: string, indent = 0): ProgressLabelType => {
+				const progressLabel = originalAdd(name, indent);
+				mockProgressLabel(progressLabel);
+				return progressLabel;
+			}) as ProgressType['add'];
+		}
+	}
+
+	const progress = new Progress();
+	vi.spyOn(progress, 'add');
+	vi.spyOn(progress, 'disable');
+	vi.spyOn(progress, 'finish');
+	vi.spyOn(progress, 'redraw');
+	vi.spyOn(progress, 'setAnsi');
+	vi.spyOn(progress, 'setHeader');
+	vi.spyOn(progress, 'write');
+
+	return {
+		Progress: vi.fn(function () {
+			return progress;
+		}),
+		default: progress,
+		ProgressLabel,
+	};
+});
+
 import progress from './progress';
 
 const PromiseFunctions = (await import('./async')).default;
